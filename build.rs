8027955706539934fb572d031a183de1cab081fe
Norbert Pozar <bertapozar@gmail.com>
2016-12-13 14:25:47 +0900

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,13 @@
 extern crate pkg_config;
 
 fn main() {
-    pkg_config::find_library("gsl").unwrap();
+    if std::process::Command::new("pkg-config").output().is_err() {
+        return;
+    }
+
+    let lib = pkg_config::probe_library("gsl").unwrap();
+
+    if lib.version.starts_with("2.") {
+        println!(r#"cargo:rustc-cfg=feature="v2""#);
+    }
 }
